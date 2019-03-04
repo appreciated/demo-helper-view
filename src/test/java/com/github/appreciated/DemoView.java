63bbe413f8db33db2f9b5393180448a6f9feeb96
Johannes Goebel <GoebelJohannes@gmx.net>
2019-03-04 18:02:01 +0100

    - updated demo

--- src/test/java/com/github/appreciated/DemoView.java
+++ src/test/java/com/github/appreciated/DemoView.java
@@ -13,6 +13,9 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import com.vaadin.flow.router.Route;
 
+import static com.github.appreciated.calc.color.helper.LumoVariables.LUMO_PRIMARY_COLOR;
+import static com.github.appreciated.calc.color.helper.LumoVariables.LUMO_PRIMARY_TEXT_COLOR;
+
 @Route("")
 //@Theme(value = Lumo.class, variant = Lumo.DARK)
 public class DemoView extends DemoHelperView {
@@ -26,23 +29,23 @@ public class DemoView extends DemoHelperView {
                         "images/demo-helper-logo.png",
                         new Button("Click Me!")
                 )
-                .withStylableDevice(getDeviceContent("< I am a stylable device, meaning that you can edit the css variables of this view by clicking on the brush button below) >"), new CssVariable("--lumo-primary-text-color"), new CssVariable("--lumo-primary-color"))
-                .withStylableDevice(new RouteBrowser(OtherContent.class), new CssVariable("--lumo-primary-text-color"), new CssVariable("--lumo-primary-color"))
-                .withStylableDevice(new RouteIFrame(OtherContent.class), new CssVariable("--lumo-primary-text-color"), new CssVariable("--lumo-primary-color"))
-                .withThemeableAndStylableDevice(getDeviceContent("< I am also a stylable device as above and also themeable meaning that the theme (Lumo.Dark and Lumo.Light) can be switched between by clicking on the switch above) >"), new CssVariable("--lumo-primary-text-color"), new CssVariable("--lumo-primary-color"))
+                .withStylableDevice(getDeviceContent("< I am a stylable device, meaning that you can edit the css variables of this view by clicking on the brush button below) >"), new CssVariable(LUMO_PRIMARY_TEXT_COLOR), new CssVariable(LUMO_PRIMARY_COLOR))
+                .withStylableDevice(new RouteBrowser(OtherContent.class), new CssVariable(LUMO_PRIMARY_TEXT_COLOR), new CssVariable(LUMO_PRIMARY_COLOR))
+                .withStylableDevice(new RouteIFrame(OtherContent.class), new CssVariable(LUMO_PRIMARY_TEXT_COLOR), new CssVariable(LUMO_PRIMARY_COLOR))
+                .withThemeableAndStylableDevice(getDeviceContent("< I am also a stylable device as above and also themeable meaning that the theme (Lumo.Dark and Lumo.Light) can be switched between by clicking on the switch above) >"), new CssVariable(LUMO_PRIMARY_TEXT_COLOR), new CssVariable(LUMO_PRIMARY_COLOR))
                 .withStylableDevice(getDeviceContent("Yay"), DeviceType.TABLET_LANDSCAPE)
                 .withDevices(
                         new TabletView(getDeviceContent("< I belong to a TabletAndPhoneView an display content inside a css rendered tablet >")).withOrientation(Orientation.LANDSCAPE),
                         new PhoneView(getDeviceContent("< I also belong to a TabletAndPhoneView an display content inside a css rendered phone >"))
                 )
                 .withParagraph("I am a ParagraphView", "I can display a header and a description")
-                .withStylableDevice(getDeviceContent("< I'm a StylablePhoneView I display content and my css variables can be edited beside me >"), new CssVariable("--lumo-primary-text-color"), new CssVariable("--lumo-primary-color"))
+                .withStylableDevice(getDeviceContent("< I'm a StylablePhoneView I display content and my css variables can be edited beside me >"), new CssVariable(LUMO_PRIMARY_TEXT_COLOR), new CssVariable(LUMO_PRIMARY_COLOR))
                 .withDevice(new PhoneView(
                                 getDeviceContent("< I belong to a PhoneView I display content inside a css rendered phone >")),
                         "Also I can show a optional message beside the PhoneView"
                 )
                 .withImage("I am a ImageParagraphView", "I can display a header, a description and an image", "images/phone.png")
-                .withStylableDevice(getDeviceContent("< I belong to a TabletView I display content inside a css rendered tablet >"), new CssVariable("--lumo-primary-text-color"))
+                .withStylableDevice(getDeviceContent("< I belong to a TabletView I display content inside a css rendered tablet >"), new CssVariable(LUMO_PRIMARY_TEXT_COLOR))
                 .withDevice(new TabletView(getDeviceContent("< I belong to a TabletView I display content inside a css rendered tablet >")))
                 .withParagraph("I am a ParagraphView",
                         "I display a header and optionally a description. Also you can add components below the description",
@@ -57,7 +60,7 @@ public class DemoView extends DemoHelperView {
                         new CodeExample("<dependency>\n" +
                                 "\t<groupId>com.github.appreciated</groupId>\n" +
                                 "\t<artifactId>demo-helper</artifactId>\n" +
-                                "\t<version>0.8.2</version>\n" +
+                                "\t<version>2.0.7</version>\n" +
                                 "</dependency>", Language.markup, "Maven")
                 )
                 .withStep("I am also a StepView",
